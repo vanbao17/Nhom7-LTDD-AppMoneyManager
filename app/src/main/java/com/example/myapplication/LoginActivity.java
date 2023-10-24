db982bdaf265b21f955abd9bb78dbcbd25aefd4f
vanbao17 <vanpao171103@gmail.com>
2023-10-24 15:08:42 +0700

    create finish login page and home page

---
+++ app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -0,0 +1,27 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+    private Button btnLogin ;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pin_entry_layout);
+        btnLogin = (Button) findViewById(R.id.login);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent  = new Intent(LoginActivity.this,PageHomeActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
